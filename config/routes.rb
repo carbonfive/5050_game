019ee7e69e480cdd6fda1fe7e4c22b5ba01ba33d
Clark Cutler <clark@carbonfive.com>
2011-11-02 18:09:19 -0700

    Splash screen stats populated by AJAX and new StatisticsController.
    - ksh_raised is hard-coded (not yet querying 50/50 API).

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Uchicken::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "content#main"
+  resources :statistics, :only => :index
 
   # See how all your routes lay out with "rake routes"
 
